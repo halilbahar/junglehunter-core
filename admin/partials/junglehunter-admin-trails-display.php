275894495171a144d6528495537eb7cb0d68b100
halilbahar <halil.s.bahar@gmail.com>
2020-05-02 19:19:22 +0200

    Create Trails form and table

--- admin/partials/junglehunter-admin-trails-display.php
+++ admin/partials/junglehunter-admin-trails-display.php
@@ -1,2 +1,95 @@
 <?php
-echo "Hello World";
+$name = '';
+$length = '';
+$route = '';
+$routes = JungleHunter_Database::junglehunter_get_routes();
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $errors = array();
+    $name = $_POST['name'];
+    if (!$name || strlen($name) == 0 || strlen($name) > 100) {
+        $errors['name'] = 'The name of the trail needs to be at least 1 and max 100 characters!';
+    }
+
+    $length = $_POST['length'];
+    if (!$length || !is_numeric(str_replace(',', '.', $length))) {
+        $errors['start'] = 'The length of the trail needs to bet set!';
+    }
+
+    $route = $_POST['route'];
+    if (!$route || in_array($route, $routes, true)) {
+        $errors['route'] = 'The route of the trail needs to be set!';
+    }
+
+    if (empty($errors)) {
+        JungleHunter_Database::junglehunter_insert_trail($name, $length, $route);
+        $name = '';
+        $length = '';
+        $route = '';
+    }
+}
+
+?>
+
+<div class="wrap">
+    <div id="junglehunter-input">
+        <h1>Insert Trail:</h1>
+        <form action="<?php menu_page_url("junglehunter-trails") ?>" method="post">
+            <div class="junglehunter-input-row">
+                <label for="junglehunter-trail-name">Name:</label>
+                <input type="text" id="junglehunter-trail-name" name="name" placeholder="A name for the Trail"
+                       value="<?php echo $name ?>">
+            </div>
+            <div class="junglehunter-input-row">
+                <label for="junglehunter-trail-length">Length:</label>
+                <input type="text" id="junglehunter-trail-length" name="length"
+                       placeholder="The length of the trail in kilometer"
+                       value="<?php echo $length ?>"
+                       oninput="this.value = this.value.replace(/[^0-9,]/g, '').replace(/(,.*),/g, '$1');">
+            </div>
+            <div class="junglehunter-input-row">
+                <label for="junglehunter-trail-route" class="junglehunter-block">Route:</label>
+                <select name="route" id="junglehunter-trail-route">
+                    <?php
+                    foreach ($routes as $single_route) {
+                        $isSelected = $single_route->route_name == $route ? 'selected' : '';
+                        echo "<option $isSelected value='$single_route->route_name'>$single_route->route_name</option>";
+                    }
+                    ?>
+                </select>
+            </div>
+            <input type="submit" value="Create">
+        </form>
+        <?php
+        if (isset($errors)) {
+            foreach ($errors as $error) {
+                echo $error . '<br>';
+            }
+        }
+        ?>
+    </div>
+    <h1>Registered Routes:</h1>
+    <table id="junglehunter-table">
+        <thead>
+        <tr>
+            <th>Id</th>
+            <th>Name</th>
+            <th>Length in kilometer</th>
+            <th>Route</th>
+        </tr>
+        </thead>
+        <tbody>
+        <?php
+        $trails = JungleHunter_Database::junglehunter_get_trails();
+        foreach ($trails as $trail) {
+            echo '<tr class="junglehunter-pointer">';
+            echo "<td>$trail->trail_id</td>";
+            echo "<td>$trail->trail_name</td>";
+            echo "<td>$trail->length</td>";
+            echo "<td>$trail->route_name</td>";
+            echo '<tr/>';
+        }
+        ?>
+        </tbody>
+    </table>
+</div>
