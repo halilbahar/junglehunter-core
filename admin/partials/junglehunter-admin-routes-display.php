6876ec2175af10d6607f7cf2a8040fed7a475c9f
halilbahar <halil.s.bahar@gmail.com>
2020-05-03 14:26:44 +0200

    Add class to the route table rows for the onclick

--- admin/partials/junglehunter-admin-routes-display.php
+++ admin/partials/junglehunter-admin-routes-display.php
@@ -87,7 +87,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         <?php
         $routes = JungleHunter_Database::junglehunter_get_routes();
         foreach ($routes as $route) {
-            echo '<tr>';
+            echo '<tr class="junglehunter-route-tr">';
             echo "<td>$route->route_name</td>";
             echo "<td>$route->start</td>";
             echo "<td>$route->url</td>";
